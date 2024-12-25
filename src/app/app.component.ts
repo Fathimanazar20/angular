b60d2b42ee744c8de87bdde24e2f45f81d4070c1
Fathimanazar20 <fathimanazar7117@gmail.com>
2024-12-25 12:05:07 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StatesComponent } from "./pages/states/states.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { NavbarComponent } from "./ui/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, StatesComponent, ContactComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { StatesComponent } from './pages/states/states.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { SingleStateComponent } from './pages/single-state/single-state.component';
+import { single } from 'rxjs';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:HomeComponent},
+    {path:'states',component:StatesComponent},
+    {path:'states/:id',component:SingleStateComponent},
+    {path:'contact',component:ContactComponent},
+   {path:'**',component:NotfoundComponent},
+   {
+    path:'single-state',component:SingleStateComponent
+   },
+
+];
